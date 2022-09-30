a0158a3001ecf117ac76789e62ecc49963e02df8
adarsh <adarsh0ranjan@gmail.com>
2022-09-30 10:16:29 +0530

    Initial Commit

---
+++ postman_demo_App/postman_demo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  postman_demo_AppApp.swift
+//  postman_demo_App
+//
+//  Created by Roro Solutions on 30/09/22.
+//
+
+import SwiftUI
+
+@main
+struct postman_demo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
